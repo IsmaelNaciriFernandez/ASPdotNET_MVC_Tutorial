0c4ac13f0be406a4a034a7d9ee3e9c031fd2aba0
IsmaelNaciriFernandez <ismaelnacirifdez@gmail.com>
2025-01-19 20:21:25 +0100

    Enough for today. Still to learn: - Razor syntax - Entity Framework Core -> Code-First - Entity Framework Core -> Database-First - CRUD

---
+++ ASPdotNET_MVC_Tutorial/Controllers/ItemsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ASPdotNET_MVC_Tutorial.Models;
+
+namespace ASPdotNET_MVC_Tutorial.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var item = new Item() { Name = "Razer Keyboard" };
+            return View(item);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content($"Id: {id}");
+        }
+
+    }
+}
